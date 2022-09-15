a4e6ec0bd252e4821afcaae7045c266f3996e59b
Bogdan <bogdan.buritch@yandex.ru>
2022-09-16 00:09:14 +0400

    add recipe methods, add ingredients filtering

---
+++ backend/api/filters.py
@@ -0,0 +1,10 @@
+from django_filters import rest_framework
+from rest_framework.filters import SearchFilter
+from recipes.models import Recipe
+
+
+class IngredientFilter(SearchFilter):
+    search_param = 'name'
+
+    def get_search_fields(self, view, request):
+        return ('^name',)
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,16 @@
-from recipes.models import Ingredient, Recipe, RecipeIngredients, Tag
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+from common.pagination import CustomPageNumberPagination
+from recipes.models import Cart, Ingredient, Recipe, RecipeIngredients, Tag, Favorite
 from rest_framework import status, viewsets
 from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.exceptions import ValidationError
 
 from .serializers import (IngredientSerializer, RecipeReadSerializer,
-                          RecipeWriteSerializer, TagSerializer)
+                          RecipeWriteSerializer, RecipeShortSerializer, TagSerializer)
+from .filters import IngredientFilter
 
 
 class TagViewSet(viewsets.ReadOnlyModelViewSet):
@@ -16,18 +23,22 @@ class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
 
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
+    filter_backends = (IngredientFilter,)
+    pagination_class = None
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
 
     queryset = Recipe.objects.all()
+    pagination_class = CustomPageNumberPagination
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
 
     def get_serializer_class(self):
         if self.request.method == 'GET':
             return RecipeReadSerializer
         return RecipeWriteSerializer
 
-    def __create_ingredients(self, recipe, ingredients):
+    def _create_ingredients(self, recipe, ingredients):
         for ingredient in ingredients:
             ingredient_obj = Ingredient.objects.get(id=ingredient['id'])
             amount = ingredient['amount']
@@ -50,7 +61,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
                 author=author, **serializer.validated_data
             )
             recipe.tags.set(tags)
-            self.__create_ingredients(recipe, ingredients)
+            ingredients = self._create_ingredients(recipe, ingredients)
 
             serializer = RecipeReadSerializer(
                 instance=recipe,
@@ -64,3 +75,59 @@ class RecipeViewSet(viewsets.ModelViewSet):
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
         )
+
+    @action(detail=True, methods=['POST', 'DELETE'], name='favorite', url_path='favorite', url_name='favorite')
+    def favorite(self, request, pk):
+        user = self.request.user
+        recipe = get_object_or_404(Recipe, pk=pk)
+        favorite = Favorite.objects.filter(user=user, recipe=recipe)
+
+        if request.method == 'POST':
+            if not favorite.exists():
+                Favorite.objects.create(user=user, recipe=recipe)
+                serializer = RecipeShortSerializer(
+                    instance=recipe,
+                    context={'request': request}
+                )
+                return Response(
+                    serializer.data,
+                    status=status.HTTP_201_CREATED
+                )
+            raise ValidationError(f'Recipe {recipe} is already in favorite.')
+
+        if favorite.exists():
+            favorite.delete()
+            return Response(
+                status=status.HTTP_204_NO_CONTENT
+            )
+        raise ValidationError(f'Recipe {recipe} is not in favorite.')
+
+    @action(detail=True,
+            methods=['POST', 'DELETE'],
+            name='shopping-cart',
+            url_path='shopping_cart',
+            url_name='shopping_cart')
+    def shopping_cart(self, request, pk):
+        user = self.request.user
+        recipe = get_object_or_404(Recipe, pk=pk)
+        cart = Cart.objects.filter(user=user, recipe=recipe)
+
+        if request.method == 'POST':
+            if not cart.exists():
+                Cart.objects.create(user=user, recipe=recipe)
+                serializer = RecipeShortSerializer(
+                    instance=recipe,
+                    context={'request': request}
+                )
+                return Response(
+                    serializer.data,
+                    status=status.HTTP_201_CREATED
+                )
+            raise ValidationError(f'Recipe {recipe} is already in cart.')
+
+        if cart.exists():
+            cart.delete()
+            return Response(
+                status=status.HTTP_204_NO_CONTENT
+            )
+        raise ValidationError(f'Recipe {recipe} is not in in cart.')
