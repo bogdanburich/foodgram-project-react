c8f665904bfbdf1aec914a158724aad88da8875a
Bogdan <bogdan.buritch@yandex.ru>
2022-09-25 17:32:42 +0400

    fix tags pagination

--- backend/api/views.py
+++ backend/api/views.py
@@ -20,6 +20,7 @@ class TagViewSet(viewsets.ReadOnlyModelViewSet):
 
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+    pagination_class = None
 
 
 class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
