05a314ae921e1508e95d098513c8d789cb748427
Bogdan <bogdan.buritch@yandex.ru>
2022-09-25 22:51:08 +0400

    update admin zone

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -3,5 +3,19 @@ from django.contrib import admin
 from .models import Ingredient, Tag
 
 admin.site.site_header = 'Foodgram Admin'
-admin.site.register(Tag)
-admin.site.register(Ingredient)
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    search_fields = ('name',)
+    list_filter = ('name', 'measurement_unit')
+    empty_value_display = '-empty-'
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'slug')
+    search_fields = ('name',)
+    list_filter = ('name', 'slug')
+    empty_value_display = '-empty-'
+
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Tag, TagAdmin)
