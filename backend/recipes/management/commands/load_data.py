25738f79c109bed03742af8a8aa1f223357c9b86
Bogdan <bogdan.buritch@yandex.ru>
2022-09-26 21:09:36 +0400

    add exception processing

--- backend/recipes/management/commands/load_data.py
+++ backend/recipes/management/commands/load_data.py
@@ -3,9 +3,10 @@ from csv import DictReader
 
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.db.utils import IntegrityError
 from django.core.management.base import BaseCommand
 
-from recipes.models import Ingredient, Tag
+from recipes.models import Ingredient, Tag, Recipe
 
 User = get_user_model()
 
@@ -15,50 +16,54 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        file_path = os.path.join(settings.TEST_DATA_DIR, 'ingredients.csv')
-        reader = DictReader(
-            open(file_path, 'r', encoding='utf-8'),
-        )
-
-        for row in reader:
-            ingredient = Ingredient(
-                name=row['name'],
-                measurement_unit=row['measurement_unit']
+        try:
+            file_path = os.path.join(settings.TEST_DATA_DIR, 'ingredients.csv')
+            reader = DictReader(
+                open(file_path, 'r', encoding='utf-8'),
             )
-            ingredient.save()
-            self.stdout.write(self.style.SUCCESS(f'Ingredient {ingredient} added'))
 
-        self.stdout.write(self.style.SUCCESS('Ingredients loaded successfully'))
+            for row in reader:
+                ingredient = Ingredient(
+                    name=row['name'],
+                    measurement_unit=row['measurement_unit']
+                )
+                ingredient.save()
+                self.stdout.write(self.style.SUCCESS(f'Ingredient {ingredient} added'))
 
-        file_path = os.path.join(settings.TEST_DATA_DIR, 'users.csv')
-        reader = DictReader(
-            open(file_path, 'r', encoding='utf-8'),
-        )
+            self.stdout.write(self.style.SUCCESS('Ingredients loaded successfully'))
 
-        for row in reader:
-            user = User.objects.create_user(
-                email=row['email'],
-                username=row['username'],
-                first_name=row['first_name'],
-                last_name=row['last_name'],
-                password=row['password'],
+            file_path = os.path.join(settings.TEST_DATA_DIR, 'users.csv')
+            reader = DictReader(
+                open(file_path, 'r', encoding='utf-8'),
             )
-            self.stdout.write(self.style.SUCCESS(f'User {user} added'))
 
-        self.stdout.write(self.style.SUCCESS('Users loaded successfully'))
+            for row in reader:
+                user = User.objects.create_user(
+                    email=row['email'],
+                    username=row['username'],
+                    first_name=row['first_name'],
+                    last_name=row['last_name'],
+                    password=row['password'],
+                )
+                self.stdout.write(self.style.SUCCESS(f'User {user} added'))
 
-        file_path = os.path.join(settings.TEST_DATA_DIR, 'tags.csv')
-        reader = DictReader(
-            open(file_path, 'r', encoding='utf-8'),
-        )
+            self.stdout.write(self.style.SUCCESS('Users loaded successfully'))
 
-        for row in reader:
-            tag = Tag(
-                name=row['name'],
-                color=row['color'],
-                slug=row['slug'],
+            file_path = os.path.join(settings.TEST_DATA_DIR, 'tags.csv')
+            reader = DictReader(
+                open(file_path, 'r', encoding='utf-8'),
             )
-            tag.save()
-            self.stdout.write(self.style.SUCCESS(f'Tag {tag} added'))
 
-        self.stdout.write(self.style.SUCCESS('Tags loaded successfully'))
+            for row in reader:
+                tag = Tag(
+                    name=row['name'],
+                    color=row['color'],
+                    slug=row['slug'],
+                )
+                tag.save()
+                self.stdout.write(self.style.SUCCESS(f'Tag {tag} added'))
+
+            self.stdout.write(self.style.SUCCESS('Tags loaded successfully'))
+
+        except IntegrityError:
+            self.stdout.write(self.style.ERROR('Data already loaded'))
