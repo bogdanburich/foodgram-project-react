65f70c765f37952c2a6765b92e03c41c45efefa2
Bogdan <bogdan.buritch@yandex.ru>
2022-09-26 14:04:07 +0400

    add tags to data load script

--- backend/recipes/management/commands/load_data.py
+++ backend/recipes/management/commands/load_data.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
 
-from recipes.models import Ingredient
+from recipes.models import Ingredient, Tag
 
 User = get_user_model()
 
@@ -15,35 +15,51 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        file_path = os.path.join(settings.TEST_DATA_DIR, 'ingredients.csv')
+        # file_path = os.path.join(settings.TEST_DATA_DIR, 'ingredients.csv')
+        # reader = DictReader(
+        #     open(file_path, 'r', encoding='utf-8'),
+        # )
+
+        # for row in reader:
+        #     ingredient = Ingredient(
+        #         name=row['name'],
+        #         measurement_unit=row['measurement_unit']
+        #     )
+        #     ingredient.save()
+        #     self.stdout.write(self.style.SUCCESS(f'Ingredient {ingredient} added'))
+
+        # self.stdout.write(self.style.SUCCESS('Ingredients loaded successfully'))
+
+        # file_path = os.path.join(settings.TEST_DATA_DIR, 'users.csv')
+        # reader = DictReader(
+        #     open(file_path, 'r', encoding='utf-8'),
+        # )
+
+        # for row in reader:
+        #     user = User(
+        #         email=row['email'],
+        #         username=row['username'],
+        #         first_name=row['first_name'],
+        #         last_name=row['last_name'],
+        #         password=row['password'],
+        #     )
+        #     user.save()
+        #     self.stdout.write(self.style.SUCCESS(f'User {user} added'))
+
+        # self.stdout.write(self.style.SUCCESS('Users loaded successfully'))
+
+        file_path = os.path.join(settings.TEST_DATA_DIR, 'tags.csv')
         reader = DictReader(
             open(file_path, 'r', encoding='utf-8'),
         )
 
         for row in reader:
-            ingredient = Ingredient(
+            tag = Tag(
                 name=row['name'],
-                measurement_unit=row['measurement_unit']
+                color=row['color'],
+                slug=row['slug'],
             )
-            ingredient.save()
-            self.stdout.write(self.style.SUCCESS(f'{ingredient} added!'))
+            tag.save()
+            self.stdout.write(self.style.SUCCESS(f'Tag {tag} added'))
 
-        self.stdout.write(self.style.SUCCESS('Ingredients loaded successfully'))
-
-        file_path = os.path.join(settings.TEST_DATA_DIR, 'users.csv')
-        reader = DictReader(
-            open(file_path, 'r', encoding='utf-8'),
-        )
-
-        for row in reader:
-            user = User(
-                email=row['email'],
-                username=row['username'],
-                first_name=row['first_name'],
-                last_name=row['last_name'],
-                password=row['password'],
-            )
-            user.save()
-            self.stdout.write(self.style.SUCCESS(f'{user} added!'))
-
-        self.stdout.write(self.style.SUCCESS('Users loaded successfully'))
+        self.stdout.write(self.style.SUCCESS('Tags loaded successfully'))
